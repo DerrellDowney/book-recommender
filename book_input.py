eb5fd573b4dbc161b4cdcb8a62430bc648b49f00
Derrell Downey Jr <derrell.downey@gmail.com>
2023-06-08 17:01:02 -0400

    added files

---
+++ book_input.py
@@ -0,0 +1,6 @@
+"""
+1) Gets title and author of book from user
+2) Sends request to database for matches of name and author
+3) Shows matches to user to select the correct one
+4) Sends the book to the algorithm
+"""
